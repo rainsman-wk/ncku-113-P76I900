3200b38c6c2e410e580bb17dc420f82a21b2d515
Rainsman <nick.wk.923@gmail.com>
2024-10-22 04:41:45 +0800

    Issue #3 Homework - Zipf Distribution Computiation
    -  Baseline version to 2.0

--- SearchEnginesApp/Properties/AssemblyInfo.cs
+++ SearchEnginesApp/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      次要版本
 //      修訂編號
 //
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: NeutralResourcesLanguage("zh-TW")]
